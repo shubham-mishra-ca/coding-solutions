d4f06f5451fa07b7451ce658893adfabb601306a
Shubham_Mishra <smishra.on.ca@gmail.com>
2022-09-14 09:39:27 -0400

    Final code with learning point for writing a function.

--- HackerRank-Python/introduction_write_a_function.py
+++ HackerRank-Python/introduction_write_a_function.py
@@ -1,16 +1,29 @@
 def is_leap(year):
+    return year%4 ==0 and (year%100!=0 or year%400==0)
+
+year = int(input())
+print(is_leap(year))
+
+
+"""Learning Point:
+Never use if-else if you\'re just return boolean value. An efficient solution is to write it in a boolean statement. 
+However note that this may not be readable if the boolean expression is complex. Don't write redundant code(one that is unnecessary and never executes) be DRY(Don't Repeat Yourself).
+
+So instead of writing this
+def is_leap(year):
     leap = False
     
     # Write your logic here
     if year%4 == 0:
         leap = True
-        if year % 100 == 0:
-            leap = False
-            if year % 400 ==0:
-                leap = True
-    
-    
+    if year % 100 == 0:
+        leap = False
+    if year % 400 ==0:
+        leap = True
     return leap
 
-year = int(input())
-print(is_leap(year))
+
+You should do something like this: (See current code)
+
+
+"""
