206f06d5032c229b3d37209830662fc887b418a0
Shubham_Mishra <smishra.on.ca@gmail.com>
2022-09-19 14:42:09 -0400

    Final code for what's your name

---
+++ HackerRank-Python/Strings/whats_your_name.py
@@ -0,0 +1,13 @@
+def print_full_name(first, last):
+    # Write your code here
+    print('Hello {0} {1}! You just delved into python.'.format(first, last))
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
+    
+'''Learning Point:
+1. Learn more about format() here - https://www.w3schools.com/python/ref_string_format.asp 
+
+'''
