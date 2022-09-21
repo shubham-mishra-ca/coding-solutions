659d1891c97f301f238cd388e84423e5a8380ccb
Shubham_Mishra <smishra.on.ca@gmail.com>
2022-09-20 23:56:15 -0400

    Final code for Pattern 2

---
+++ Miscellaneous-Coding-Python/Patterns/pattern2.py
@@ -0,0 +1,21 @@
+'''
+Pattern
+    *
+    **
+    ***
+    ****
+    *****
+
+'''
+
+def pattern2(n):
+    #Outer loop is for the number of rows in the pattern.
+    for i in range(1,n+1):
+        #Inner loop for the number of columns for each row.
+        for j in range(i):
+            print("*",end="")
+        print()
+
+
+n = int(input().strip())
+pattern1(n)
